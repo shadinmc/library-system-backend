e7350387911ff40291bba7c4a017952ca666e8cf
Shadin mc <Shadinmc2002@gmail.com>
2026-01-06 22:12:19 +0530

    Add JPA entities for users and books

---
+++ src/main/java/com/example/library/entity/BookEntity.java
@@ -0,0 +1,63 @@
+package com.example.library.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(unique = true)
+    private String isbn;
+
+    // Constructors
+    public BookEntity() {}
+
+    public BookEntity(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
